6b9ec1cda00a91e9f0562d1752770a7c7337e5ab
martinRenou <martin.renou@gmail.com>
2025-06-27 10:07:13 +0200

    Iterate

--- packages/mambajs-core/src/helper.ts
+++ packages/mambajs-core/src/helper.ts
@@ -42,7 +42,7 @@ export interface IEmpackEnvMetaMountPoint {
 export interface IEmpackEnvMeta {
   prefix: string;
   packages: IEmpackEnvMetaPkg[];
-  mounts: IEmpackEnvMetaMountPoint[];
+  mounts?: IEmpackEnvMetaMountPoint[];
 }
 
 /**
