0851ee4e86db1f17a6368211ce00075a482b10fc
martinRenou <martin.renou@gmail.com>
2025-07-10 12:22:50 +0200

    Rename folder plus some CI tweaks

--- unittests/runner.js
+++ unittests/runner.js
@@ -6,7 +6,7 @@ import { fileURLToPath } from 'url';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-const testsDir = path.resolve(__dirname, 'testlib', 'tests');
+const testsDir = path.resolve(__dirname, 'testlib', 'unittests');
 
 function runTests(dir) {
   const files = fs.readdirSync(dir);
