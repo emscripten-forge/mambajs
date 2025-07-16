c743a61bb305d1be3f114c43ac0687149388e2bf
martinRenou <martin.renou@gmail.com>
2025-07-16 10:26:03 +0200

    Pip list test

--- unittests/tests/parser/test-parse-list.ts
+++ unittests/tests/parser/test-parse-list.ts
@@ -3,10 +3,10 @@ import { expect } from 'earl';
 
 let cmd = parse('%conda list');
 
-// expect(cmd.commands[0].data.type).toEqual('conda');
+expect(cmd.commands[0].data.type).toEqual('conda');
 expect(cmd.commands[0].type).toEqual('list');
 
 cmd = parse('%pip list');
 
-// expect(cmd.commands[0].data.type).toEqual('pip');
+expect(cmd.commands[0].data.type).toEqual('pip');
 expect(cmd.commands[0].type).toEqual('list');
