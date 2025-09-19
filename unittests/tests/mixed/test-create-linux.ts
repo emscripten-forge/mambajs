41f7cc8f2beeae9f56cdce6bba9c08f4a4cd0403
martinRenou <martin.renou@gmail.com>
2025-09-19 16:02:54 +0200

    Add platform option to `create`

--- unittests/tests/mixed/test-create-linux.ts
+++ unittests/tests/mixed/test-create-linux.ts
@@ -6,36 +6,26 @@ const logger = new TestLogger();
 
 const yml = `
 channels:
-  - emscripten-forge
-  - conda-forge
+  - https://prefix.dev/conda-forge
 dependencies:
   - pandas
   - xeus-python
   - ipycanvas=0.13.2
-  - pip:
-    - ipydatagrid
-    - bqplot ==0.12.42
 `;
 
-create({yml, logger}).then(async result => {
+create({yml, logger, platform: "linux-64"}).then(async result => {
   const condaPackageNames = Object.values(result.packages).map(pkg => pkg.name);
-  const pipPackageNames = Object.values(result.pipPackages).map(pkg => pkg.name);
 
-  // Index by package name for convenienve
+  // Index by package name for convenience
   const condaPackages: { [key: string]: ISolvedPackage } = {};
   Object.keys(result.packages).map(filename => {
     condaPackages[result.packages[filename].name] =
       result.packages[filename];
   });
-  const pipPackages: { [key: string]: ISolvedPipPackage } = {};
-  Object.keys(result.pipPackages).map(filename => {
-    pipPackages[result.pipPackages[filename].name] =
-      result.pipPackages[filename];
-  });
+
+  expect(result.platform).toEqual('linux-64');
 
   expect(condaPackageNames).toInclude('xeus-python', 'xeus-python-shell', 'pandas', 'ipycanvas', 'ipywidgets');
-  expect(pipPackageNames).toInclude('bqplot', 'ipydatagrid');
 
   expect(condaPackages['ipycanvas'].version).toEqual('0.13.2');
-  expect(pipPackages['bqplot'].version).toEqual('0.12.42');
 });
