00953d7d70302b562b390ccc4dc86937e3ebe9f2
martinRenou <martin.renou@gmail.com>
2025-11-13 15:46:47 +0100

    Fix handling PyPi source packages for other platforms than emscripten

--- unittests/tests/mixed/test-create-linux.ts
+++ unittests/tests/mixed/test-create-linux.ts
@@ -8,9 +8,15 @@ const yml = `
 channels:
   - https://prefix.dev/conda-forge
 dependencies:
-  - pandas
+  - python
   - xeus-python
+  - pandas
   - ipycanvas=0.13.2
+  - bzip2=1.0.8
+  - xz=5.2.6
+  - pip:
+    - starlette==0.17.1
+    - Checkm==0.4
 `;
 
 create({yml, logger, platform: "linux-64"}).then(async result => {
@@ -28,4 +34,5 @@ create({yml, logger, platform: "linux-64"}).then(async result => {
   expect(condaPackageNames).toInclude('xeus-python', 'xeus-python-shell', 'pandas', 'ipycanvas', 'ipywidgets');
 
   expect(condaPackages['ipycanvas'].version).toEqual('0.13.2');
+
 });
