a03d99e2916f4ac116efda34f40c6bc88ae830f6
martinRenou <martin.renou@gmail.com>
2026-01-29 16:07:09 +0100

    Mambajs CLI

---
+++ packages/mambajs-cli/esbuild.config.js
@@ -0,0 +1,40 @@
+const esbuild = require("esbuild");
+
+(async () => {
+  try {
+    await esbuild.build({
+      entryPoints: ["./src/index.ts"],
+      bundle: true,
+
+      tsconfig: "./tsconfig.json",
+
+      // Node CLI target
+      platform: "node",
+      target: "node18",
+
+      // Output
+      outdir: "./dist",
+      format: "esm",
+
+      // Keep CLI runnable
+      banner: {
+        js: "#!/usr/bin/env node"
+      },
+
+      external: [
+        "fs",
+        "path",
+        "os",
+        "child_process",
+        "crypto"
+      ],
+
+      sourcemap: false,
+      minify: false,
+
+      logLevel: "info",
+    });
+  } catch (err) {
+    process.exit(1);
+  }
+})();
---
+++ packages/mambajs-cli/src/index.ts
@@ -0,0 +1,41 @@
+import { readFileSync, writeFileSync } from "fs";
+import process from "process";
+
+import { Platform } from '@conda-org/rattler';
+
+import { create } from "@emscripten-forge/mambajs";
+
+
+async function main() {
+  const [, , command, envPath, outputPath, platform] = process.argv;
+
+  let targetPlatform = platform as Platform | undefined;
+
+  if (command !== "create-lock" || !envPath || !outputPath) {
+    console.error("Usage: mambajs create-lock environment.yml lock.json");
+    process.exit(1);
+  }
+
+  if (!targetPlatform) {
+    targetPlatform = 'emscripten-wasm32';
+  }
+
+  const environmentYml = readFileSync(envPath, "utf8");
+
+  console.log("Solving environment...");
+
+  const lock = await create({
+    yml: environmentYml,
+    logger: console,
+    platform: targetPlatform
+  });
+
+  writeFileSync(outputPath, JSON.stringify(lock, null, 2));
+
+  console.log(`Lockfile successfully written to ${outputPath}`);
+}
+
+main().catch((err) => {
+  console.error(err);
+  process.exit(1);
+});
