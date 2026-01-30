f6a0f45e1db71e060dd5704a7dbb56d5b9140a6a
martinRenou <martin.renou@gmail.com>
2026-01-30 10:01:52 +0100

    Add CLI tests

---
+++ tests/test_cli.py
@@ -0,0 +1,15 @@
+import subprocess
+from pathlib import Path
+import sys
+import shutil
+
+HERE = Path(__file__).parent
+
+def test_create_lock_from_environment_file():
+    lockfile = "lock.json"
+
+    Path(HERE / lockfile).unlink(missing_ok=True)
+
+    subprocess.run(["mambajs", "create-lock", "test-env.yml", lockfile], check=True)
+
+    assert Path(HERE / lockfile).exists()
