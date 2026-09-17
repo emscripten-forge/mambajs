132d5908872f8b9a784e53e43a334faff0b2122f
martinRenou <martin.renou@gmail.com>
2026-09-17 11:52:26 +0200

    Simplify test + fix other tests

--- unittests/tests/pip/test-pyemscripten-platform.ts
+++ unittests/tests/pip/test-pyemscripten-platform.ts
@@ -19,18 +19,6 @@ create({yml, logger}).then(async result => {
   const condaPackageNames = Object.values(result.packages).map(pkg => pkg.name);
   const pipPackageNames = Object.values(result.pipPackages).map(pkg => pkg.name);
 
-  // Index by package name for convenienve
-  const condaPackages: { [key: string]: ISolvedPackage } = {};
-  Object.keys(result.packages).map(filename => {
-    condaPackages[result.packages[filename].name] =
-      result.packages[filename];
-  });
-  const pipPackages: { [key: string]: ISolvedPipPackage } = {};
-  Object.keys(result.pipPackages).map(filename => {
-    pipPackages[result.pipPackages[filename].name] =
-      result.pipPackages[filename];
-  });
-
   expect(condaPackageNames).toInclude('xeus-python', 'numpy');
   expect(pipPackageNames).toInclude('contourpy');
 });
